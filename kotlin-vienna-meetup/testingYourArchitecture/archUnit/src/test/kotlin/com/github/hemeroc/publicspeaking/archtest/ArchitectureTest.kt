e71d526e349e2e8f3ab4f7f1963dbced645cef62
Mario Fleischhacker <mario.fleischhacker@gmail.com>
2025-01-23 11:15:56 +0100

    feat: add layered test sample

--- kotlin-vienna-meetup/testingYourArchitecture/archUnit/src/test/kotlin/com/github/hemeroc/publicspeaking/archtest/ArchitectureTest.kt
+++ kotlin-vienna-meetup/testingYourArchitecture/archUnit/src/test/kotlin/com/github/hemeroc/publicspeaking/archtest/ArchitectureTest.kt
@@ -3,8 +3,9 @@ package com.github.hemeroc.publicspeaking.archtest
 import com.tngtech.archunit.core.domain.JavaClasses
 import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTest
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
+import com.tngtech.archunit.library.Architectures.layeredArchitecture
 
 
 @AnalyzeClasses(packages = ["com.github.hemeroc.publicspeaking.archtest"])
@@ -14,23 +15,42 @@ internal class ArchitectureTest {
         val inlineRule = classes()
             .that().resideInAPackage("..service..")
             .should().onlyBeAccessed().byAnyPackage("..application..", "..service..")
-
         inlineRule.check(importedClasses)
     }
 
     @ArchTest
     fun `concrete io components should only be accessed in the application package`(importedClasses: JavaClasses) {
-        IO_ACCESS_RULE.check(importedClasses)
+        classes()
+            .that().resideInAPackage("..io.impl..")
+            .should().onlyBeAccessed().byAnyPackage("..application..")
+            .check(importedClasses)
     }
 
     @ArchTest
     fun `io should not dependent on other io`(importedClasses: JavaClasses) {
-        ArchRuleDefinition.noClasses().that().resideInAPackage("..io.impl..")
+        noClasses().that().resideInAPackage("..io.impl..")
             .should().dependOnClassesThat().resideInAPackage("..io.impl..")
+            .check(importedClasses)
+    }
+
+    @ArchTest
+    fun `ensure that architecture layers are not mixed`(importedClasses: JavaClasses) {
+        LAYERS
+            .whereLayer(MAIN).mayNotBeAccessedByAnyLayer()
+            .whereLayer(SERVICE).mayOnlyBeAccessedByLayers(MAIN, SERVICE)
+            .whereLayer(IO).mayOnlyBeAccessedByLayers(MAIN)
             .check(importedClasses)
     }
 }
 
-private val IO_ACCESS_RULE = classes()
-    .that().resideInAPackage("..io.impl..")
-    .should().onlyBeAccessed().byAnyPackage("..application..")
+const val SERVICE = "Service"
+const val IO = "IO"
+const val MODEL = "Model"
+const val MAIN = "Main"
+
+private val LAYERS = layeredArchitecture()
+    .consideringAllDependencies()
+    .layer(SERVICE).definedBy("..service..")
+    .layer(IO).definedBy("..io..")
+    .layer(MODEL).definedBy("..model..")
+    .layer(MAIN).definedBy("..application..")
